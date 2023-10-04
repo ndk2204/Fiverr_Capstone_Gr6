5be528821f1370508edbfe65190e51e0f3a2d90c
Khanh <ndk2204@gmail.com>
2023-10-05 01:48:33 +0700

    hover

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,92 +1,56 @@
-import React, { useState, useEffect } from "react";
-import type { MenuProps } from "antd";
-import { Menu } from "antd";
+import React, { useEffect } from "react";
 import css from "./nav.module.scss";
+import { Link } from "react-router-dom";
 import { axiosWithAuth } from "src/services/config.service";
-import { Card } from "../card";
-
-const kkk = {
-  a: 222,
-  b: 333,
-};
-
-const items: MenuProps["items"] = [
-  {
-    label: "Navigation Three - Menu",
-    key: "menu",
-    children: [
-      {
-        type: "group",
-        label: `${kkk.a}`,
-        children: [
-          {
-            label: "Option 1",
-            key: "setting:1",
-          },
-          {
-            label: "Option 2",
-            key: "setting:2",
-          },
-        ],
-      },
-      {
-        type: "group",
-        label: `${kkk.a}`,
-        children: [
-          {
-            label: "Option 1",
-            key: "setting:1",
-          },
-          {
-            label: "Option 2",
-            key: "setting:2",
-          },
-        ],
-      },
-      {
-        type: "group",
-        label: "Item 2",
-        children: [
-          {
-            label: "Option 3",
-            key: "setting:3",
-          },
-          {
-            label: "Option 4",
-            key: "setting:4",
-          },
-        ],
-      },
-    ],
-  },
-];
+import { useAppSelector } from "src/redux/config-store";
+import { useDispatch } from "react-redux";
+import { setListMenu } from "src/redux/slices/product.slice";
 
 function MenuNav() {
-  const [current, setCurrent] = useState("mail");
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
+  const listMenu = useAppSelector((state) => {
+    return state.productReducer.listMenu;
+  });
+  const dispatch = useDispatch();
+
   useEffect(() => {
     (async () => {
       const resp = await axiosWithAuth({
-        method: "get",
+        // method: "get",
         headers: {
-          'tokenCybersoft': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MDA4IiwiSGV0SGFuU3RyaW5nIjoiMjgvMDEvMjAyNCIsIkhldEhhblRpbWUiOiIxNzA2NDAwMDAwMDAwIiwibmJmIjoxNjc3NDMwODAwLCJleHAiOjE3MDY1NDc2MDB9.eo3y0MmcjE8Jl4fRzUJLBoZzylEeFDcUTfWXvtb1hdc` 
-        } ,
+          tokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MDA4IiwiSGV0SGFuU3RyaW5nIjoiMjgvMDEvMjAyNCIsIkhldEhhblRpbWUiOiIxNzA2NDAwMDAwMDAwIiwibmJmIjoxNjc3NDMwODAwLCJleHAiOjE3MDY1NDc2MDB9.eo3y0MmcjE8Jl4fRzUJLBoZzylEeFDcUTfWXvtb1hdc`,
+        },
       });
-      console.log(resp.data);
-      return resp.data;
+      const action = setListMenu(resp.data.content);
+      dispatch(action);
     })();
   }, []);
+  console.log(listMenu);
 
   return (
-    <Menu
-      onClick={onClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      items={items}
-    />
+    <ul className={css.nav}>
+      {listMenu.map((item: any, index: number) => {
+        return (
+          <div  className="menuDrop" key={index}>
+            <div>{item.tenLoaiCongViec}</div>
+              {item.dsNhomChiTietLoai.map((sub: any, number: number) => {
+                return (
+                  <div key={number} className="dropDown">
+                    <a>{sub.tenNhom}</a>
+
+                    {sub.dsChiTietLoai.map((subMenu: any, stt: number) => {
+                      return (
+                        <p key={stt} className={css.sub}>
+                          {subMenu.tenChiTiet}
+                        </p>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+          </div>
+        );
+      })}
+    </ul>
   );
 }
 
--- src/redux/slices/product.slice.ts
+++ src/redux/slices/product.slice.ts
@@ -6,14 +6,15 @@ import { getLocalStorage } from "src/utils";
 type TState = {
   listProduct: TCardItem[];
   gioHang: TCardItem[];
-
+  listMenu: any;
 };
 
 let dataLocal = getLocalStorage("localCarts");
 
 const initialState: TState = {
   listProduct: [],
-  gioHang :dataLocal
+  gioHang :dataLocal,
+  listMenu:[],
 };
 
 const productSlice = createSlice({
@@ -22,19 +23,21 @@ const productSlice = createSlice({
   reducers: {
     // name + reducer: productSlice/setListProduct
     setListProduct: (state, action) => {
-  
       state.listProduct = action.payload;
-      
     },
+
     setgioHang: (state, action) => {
       state.gioHang = action.payload;
     },
 
+    setListMenu: (state, action) => {
+      state.listMenu = action.payload;
+    },
   },
 });
 
 // Bóc tách ra action creator của Redux
-export const { setListProduct,setgioHang } = productSlice.actions;
+export const { setListProduct,setgioHang,setListMenu } = productSlice.actions;
 
 //productSlice tạo ra hàm reducer, chấm đến để sử dụng reducer
 export default productSlice.reducer;
